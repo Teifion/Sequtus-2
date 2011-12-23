937512a5eb6af749715d0cb58c5c19f0c5fa9db1
Teifion <sarkalian@gmail.com>
2011-12-23 22:49:47 +0000

    Removed battle_network

--- sequtus/game/battle_network.py
+++
@@ -1,8 +0,0 @@
-from __future__ import division
-
-from sequtus.game import battle_io
-
-class BattleNetwork (battle_io.BattleIO):
-    pass
-
-
--- sequtus/tests/battle_network_t.py
+++
@@ -1,9 +0,0 @@
-import pygame
-import unittest
-
-from sequtus.tests.network_tests import dummy_player
-
-class BattleNetworkTests(unittest.TestCase):
-    pass
-
-suite = unittest.TestLoader().loadTestsFromTestCase(BattleNetworkTests)
